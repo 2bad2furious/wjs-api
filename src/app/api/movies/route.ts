74dcc3698f02d78918df5c405b47e7448b91c427
2bad2furious <22004964+2bad2furious@users.noreply.github.com>
2024-01-11 16:15:26 +0100

    Add rating filters

--- src/app/api/movies/route.ts
+++ src/app/api/movies/route.ts
@@ -8,9 +8,20 @@ export async function GET(request: Request) {
     const url = new URL(request.url);
     const search = url.searchParams.get("search");
     const type = url.searchParams.get("type");
+    const minRatingRaw = url.searchParams.get("minRating")
+    const minRating = minRatingRaw !== null ? Number(minRatingRaw) : null;
+    if (typeof minRating === "number" && isNaN(minRating)) {
+        return Response.json("Invalid minRating", {status: 400});
+    }
+    const maxRatingRaw = url.searchParams.get("maxRating")
+    const maxRating = maxRatingRaw !== null ? Number(maxRatingRaw) : null;
+    if (typeof maxRating === "number" && isNaN(maxRating)) {
+        return Response.json("Invalid maxRating", {status: 400});
+    }
 
 
-    const users = filterMovies(search, type);
+
+    const users = filterMovies(search, type, minRating, maxRating);
     const headers = new Headers({
         "Access-Control-Allow-Origin": "*"
     });
@@ -18,8 +29,8 @@ export async function GET(request: Request) {
     return Response.json(users, {headers});
 }
 
-function filterMovies(search: string | null, type: string | null) {
-    if (!search && !type) {
+function filterMovies(search: string | null, type: string | null, minRating: number | null, maxRating: number | null) {
+    if (!search && !type && minRating === null && maxRating === null) {
         return movies.slice(0, MAX_SIZE);
     }
 
@@ -28,7 +39,7 @@ function filterMovies(search: string | null, type: string | null) {
 
     const results: Movie[] = [];
     for (const movie of movies) {
-        const {type, title} = movie;
+        const {type, title, imdbScore} = movie;
         if (results.length === MAX_SIZE) {
             break;
         }
@@ -37,6 +48,16 @@ function filterMovies(search: string | null, type: string | null) {
             continue;
         }
         if (searchLowerCase && !title.toLowerCase().includes(searchLowerCase)) {
+            continue;
+        }
+        if (searchLowerCase && !title.toLowerCase().includes(searchLowerCase)) {
+            continue;
+        }
+
+        if (minRating !== null && imdbScore < minRating) {
+            continue;
+        }
+        if (maxRating !== null && imdbScore > maxRating) {
             continue;
         }
         results.push(movie);
